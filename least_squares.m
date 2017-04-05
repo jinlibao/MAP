ae0fb1fe1176fba71ab78c6424bfd762e11aa55e
Libao Jin <jinlibao@outlook.com>
2017-04-05 15:30:01 -0600

    Update function load_data().

--- least_squares.m
+++ least_squares.m
@@ -23,7 +23,7 @@ function [t, S] = load_data(filename)
 % Output argument: data
 % data is an N by 2 matrix, where N is the number of time intervals.
 % data[:, 1], the first column of data is time intervals.
-% data[:, 2], the second column of data is cumulative volume of oil produced.
+% data[:, 2], the second column of data is saturation of water.
 
 % Set default input arguments: filename
 if nargin == 0
@@ -33,7 +33,7 @@ end
 % Read data from excel file
 raw_data = xlsread(filename, 'Data', 'A1:D49');
 t = raw_data(:, 1); % Time Interval
-S = cumsum(raw_data(:, 3)); % Cumulative Volume of Oil Produced
+S = raw_data(:, 2) / (raw_data(:, 2) + raw_data(:, 3)); % Saturation of Water
 end
 
 function [c, S_hat] = solve_least_square(t, S, TYPE, N, ALPHA)
